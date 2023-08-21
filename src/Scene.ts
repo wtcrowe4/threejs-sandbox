549514fccff57393e99625510e1ef3de2b976c0e
Thomas Crowe <wtcrowe4@outlook.com>
2023-08-21 07:36:17 -0400

    Starting on input/bullets

--- src/Scene.ts
+++ src/Scene.ts
@@ -1,12 +1,22 @@
 import * as THREE from 'three';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
+import Bullet from './Bullet.ts';
 
 export default class Scene extends THREE.Scene 
 {
     private readonly mtlLoader = new MTLLoader();
     private readonly objLoader = new OBJLoader();
     private readonly camera: THREE.PerspectiveCamera;
+    private readonly keyDown = new Set<string>();
+    
+    private blaster?: THREE.Group
+	private bulletMtl?: MTLLoader.MaterialCreator
+
+	private directionVector = new THREE.Vector3()
+
+	private bullets: Bullet[] = []
+	private targets: THREE.Group[] = []
 
     constructor(camera: THREE.PerspectiveCamera) {
         super();
@@ -14,7 +24,7 @@ export default class Scene extends THREE.Scene
     }    
     
     async Initialize() {
-        
+        //objefcts
         const target1 = await this.createTarget();
         target1.position.set(-1.5, 1, 0);
         const target2 = await this.createTarget();
@@ -32,10 +42,14 @@ export default class Scene extends THREE.Scene
         this.add(target1, target2, target3, target4)
         this.add(blaster);
 
-
+        //lighting
         const light = new THREE.DirectionalLight(0xffffff, 1);
         light.position.set(0, 5, 5);
         this.add(light);
+
+        //event listeners
+        window.addEventListener('keydown', (e) => this.handleKeyDown.add(e.key));
+        window.addEventListener('keyup', (e) => this.handleKeyUp.add(e.key));
     }
 
     private async createTarget() {
@@ -54,6 +68,73 @@ export default class Scene extends THREE.Scene
         const blasterObj = await this.objLoader.loadAsync('assets/blasterB.obj');
         return blasterObj;
     }
+
+    private handleKeyDown(e: KeyboardEvent) {
+        this.keyDown.add(e.key.toLowerCase());
+    }
+
+    private handleKeyUp(e: KeyboardEvent) {
+        this.keyDown.delete(e.key.toLowerCase());
+    }
+
+
+    private handleInput() {
+            if (!this.blaster)
+            {
+                return
+            }
+    
+            const shiftKey = this.keyDown.has('shift')
+    
+            if (!shiftKey)
+            {
+                if (this.keyDown.has('a') || this.keyDown.has('arrowleft'))
+                {
+                    this.blaster.rotateY(0.02)
+                }
+                else if (this.keyDown.has('d') || this.keyDown.has('arrowright'))
+                {
+                    this.blaster.rotateY(-0.02)
+                }
+            }
+    
+            const dir = this.directionVector
+    
+            this.camera.getWorldDirection(dir)
+    
+            const speed = 0.1
+    
+            if (this.keyDown.has('w') || this.keyDown.has('arrowup'))
+            {
+                this.blaster.position.add(dir.clone().multiplyScalar(speed))
+            }
+            else if (this.keyDown.has('s') || this.keyDown.has('arrowdown'))
+            {
+                this.blaster.position.add(dir.clone().multiplyScalar(-speed))
+            }
+    
+            if (shiftKey)
+            {
+                const strafeDir = dir.clone()
+                const upVector = new THREE.Vector3(0, 1, 0)
+    
+                if (this.keyDown.has('a') || this.keyDown.has('arrowleft'))
+                {
+                    this.blaster.position.add(
+                        strafeDir.applyAxisAngle(upVector, Math.PI * 0.5)
+                            .multiplyScalar(speed)
+                    )
+                }
+                else if (this.keyDown.has('d') || this.keyDown.has('arrowright'))
+                {
+                    this.blaster.position.add(
+                        strafeDir.applyAxisAngle(upVector, Math.PI * -0.5)
+                            .multiplyScalar(speed)
+                    )
+                }
+            }
+    }
+    
 
 
     Update() {
