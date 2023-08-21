4db525020ce75537a459be3c89f590d974b5c9f1
Thomas Crowe <wtcrowe4@outlook.com>
2023-08-21 05:06:18 -0400

    override margin from user agent css

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 100);
 const renderer = new THREE.WebGLRenderer();
 
 renderer.setSize(window.innerWidth, window.innerHeight);
