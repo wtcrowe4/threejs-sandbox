b95fb2dd7297c31ca2b06e99ed4e1184950ef436
Thomas Crowe <wtcrowe4@outlook.com>
2023-08-27 12:58:20 -0400

    Simple hole layout

--- src/PuttPutt/Hole.ts
+++ src/PuttPutt/Hole.ts
@@ -12,6 +12,7 @@ export default class Hole extends THREE.Group {
 
     public async createHole() {
         
+        //Hole Start
         const holeStartMtl = await this.mtlLoader.loadAsync('assets/PuttPutt/Hole/side.mtl');
         holeStartMtl.preload();
         const holeStart = await this.objLoader.setMaterials(holeStartMtl).loadAsync('assets/PuttPutt/Hole/side.obj');
@@ -33,7 +34,52 @@ export default class Hole extends THREE.Group {
         holeStartLeft.position.set(-1, -.2, -.2);
         this.hole?.push(holeStartLeft);
 
-        
+
+        //Hole Layout
+        //Right
+        const holeLength = 5;
+        const holeSideRightMtl = await this.mtlLoader.loadAsync('assets/PuttPutt/Hole/side.mtl');
+        holeSideRightMtl.preload();
+        //const holeSideRight = await this.objLoader.setMaterials(holeSideRightMtl).loadAsync('assets/PuttPutt/Hole/side.obj');
+        //holeSideRight.position.set(1, -.2, -1.2);
+        //this.hole?.push(holeSideRight);
+        for(let i=0; i<=holeLength; i++){
+            const holeSideRight = await this.objLoader.setMaterials(holeSideRightMtl).loadAsync('assets/PuttPutt/Hole/side.obj');
+            holeSideRight.position.set(1, -.2, -1.2 - i);
+            this.hole?.push(holeSideRight);
+        }
+
+        //Middle
+        const holeMiddleMtl = await this.mtlLoader.loadAsync('assets/PuttPutt/Hole/open.mtl');
+        holeMiddleMtl.preload();
+        //const holeMiddle = await this.objLoader.setMaterials(holeMiddleMtl).loadAsync('assets/PuttPutt/Hole/open.obj');
+        //holeMiddle.position.set(0, -.2, -1.2);
+        //this.hole?.push(holeMiddle);
+        for(let i=0; i<=holeLength; i++){
+            const holeMiddle = await this.objLoader.setMaterials(holeMiddleMtl).loadAsync('assets/PuttPutt/Hole/open.obj');
+            holeMiddle.position.set(0, -.2, -1.2 - i);
+            this.hole?.push(holeMiddle);
+        }
+
+        //Left
+        const holeSideLeftMtl = await this.mtlLoader.loadAsync('assets/PuttPutt/Hole/side.mtl');
+        holeSideLeftMtl.preload();
+        //const holeSideLeft = await this.objLoader.setMaterials(holeSideLeftMtl).loadAsync('assets/PuttPutt/Hole/side.obj');
+        //holeSideLeft.rotateOnAxis(new THREE.Vector3(0, -1, 0), Math.PI / 2);
+        //holeSideLeft.position.set(-1, -.2, -1.2);
+        //this.hole?.push(holeSideLeft);
+        for(let i=0; i<=holeLength; i++){
+            const holeSideLeft = await this.objLoader.setMaterials(holeSideLeftMtl).loadAsync('assets/PuttPutt/Hole/side.obj');
+            holeSideLeft.rotateOnAxis(new THREE.Vector3(0, -1, 0), Math.PI);
+            holeSideLeft.position.set(-1, -.2, -1.2 - i);
+            this.hole?.push(holeSideLeft);
+        }
+
+
+
+        //Hole End
+
+
         
         return this.hole;
     }
