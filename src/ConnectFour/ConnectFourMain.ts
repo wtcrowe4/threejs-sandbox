f4191a6b8b8f8c35cbbb0461aa74b004ab786dfd
Thomas Crowe <wtcrowe4@outlook.com>
2023-09-27 16:09:40 -0400

    clean up

--- src/ConnectFour/ConnectFourMain.ts
+++ src/ConnectFour/ConnectFourMain.ts
@@ -49,90 +49,17 @@ export default class ConnectFourGame {
     player2: string = "black";
     currentPlayer: string = this.player1;
 
-    // public TakeTurn(player: string, column: number) {
-    //     // Player drops a piece into the specified column
-    //     this.DropPiece(player, column);
+    public SwitchPlayer() {
+        if (this.currentPlayer == this.player1) {
+            this.currentPlayer = this.player2;
+        }
+        else {
+            this.currentPlayer = this.player1;
+        }
+    }
 
-    //     // Check if the current player has won the game
-    //     if (this.CheckWin(player)) {
-    //         // If the player has won, print a message and reset the game
-    //         console.log(`${player} has won the game!`);
-    //         this.Reset();
-    //     }
-    // }
-
-    // public DropPiece(player: string, column: number) {
-    //     // Check if the column number is valid
-    //     if (column < 0 || column >= this.board?.columns) {
-    //         console.error("Invalid column number");
-    //         return;
-    //     }
-
-    //     // Iterate from the bottom of the specified column upwards
-    //     for (let row = this.board?.rows! - 1; row >= 0; row--) {
-    //         // If an empty spot is found, place the player's piece there
-    //         if (this.board?.grid[row][column] === null) {
-    //             this.board.grid[row][column] = player;
-
-    //             // Switch the current player
-    //             this.currentPlayer = this.currentPlayer === this.player1 ? this.player2 : this.player1;
-    //             return;
-    //         }
-    //     }
-
-    //     // If no empty spot is found, the column is full
-    //     console.error("Column is full");
-    // }
-
-    // public CheckWin(player: string) {
-    //     //check for win
-    //     // Check for horizontal win
-    //     for (let row = 0; row < this.board?.rows!; row++) {
-    //         for (let column = 0; column < this.board?.columns! - 3; column++) {
-    //             if (this.board?.grid[row][column] === player &&
-    //                 this.board?.grid[row][column + 1] === player &&
-    //                 this.board?.grid[row][column + 2] === player &&
-    //                 this.board?.grid[row][column + 3] === player) {
-    //                 return true;
-    //             }
-    //         }
-    //     }
-    //     // Check for vertical win
-    //     for (let row = 0; row < this.board?.rows! - 3; row++) {
-    //         for (let column = 0; column < this.board?.columns!; column++) {
-    //             if (this.board?.grid[row][column] === player &&
-    //                 this.board?.grid[row + 1][column] === player &&
-    //                 this.board?.grid[row + 2][column] === player &&
-    //                 this.board?.grid[row + 3][column] === player) {
-    //                 return true;
-    //             }
-    //         }
-    //     }
-    //     // Check for diagonal win (positive slope)
-    //     for (let row = 0; row < this.board?.rows! - 3; row++) {
-    //         for (let column = 0; column < this.board?.columns! - 3; column++) {
-    //             if (this.board?.grid[row][column] === player &&
-    //                 this.board?.grid[row + 1][column + 1] === player &&
-    //                 this.board?.grid[row + 2][column + 2] === player &&
-    //                 this.board?.grid[row + 3][column + 3] === player) {
-    //                 return true;
-    //             }
-    //         }
-    //     }
-    //     return false;
-    // }
 
     
-
-     
-
-    public Reset() {
-        //reset board
-        this.board = new ConnectFourBoard(this.scene.rows, this.scene.columns);
-        //reset player
-        this.currentPlayer = this.player1;
-
-    }
 
     
 
