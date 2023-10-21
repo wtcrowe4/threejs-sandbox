e8f3df6787108e31ef742309604502b1783bbf48
Thomas Crowe <wtcrowe4@outlook.com>
2023-10-21 12:08:36 -0400

    init methods

--- src/AirHockey/AirHockeyTable.ts
+++ src/AirHockey/AirHockeyTable.ts
@@ -27,6 +27,16 @@ export default class AirHockeyTable {
 
     constructor(scene: AirHockeyScene) {
         this.scene = scene;
+        this.initializeTable();
+        this.initializePieces();
+        
+    }
+
+    public GetTable() {
+        return this.table;
+    }
+
+    public initializeTable() {
         this.tableWidth = 10;
         this.tableHeight = 1;
         this.tableDepth = 10;
@@ -40,10 +50,24 @@ export default class AirHockeyTable {
         this.table = new THREE.Mesh(this.tableGeometry, this.tableMaterial);
         this.table.position.set(this.tableX, this.tableY, this.tableZ);
         this.scene.add(this.table);
-
+        
     }
 
-    public GetTable() {
-        return this.table;
+    public initializePieces() {
+        this.puckGeometry = new THREE.SphereGeometry(.5, 32, 32);
+        this.puckMaterial = new THREE.MeshBasicMaterial({ color: "black" });
+        this.puck = new THREE.Mesh(this.puckGeometry, this.puckMaterial);
+        this.puck.position.set(0, 0, 0);
+        this.scene.add(this.puck);
+        this.hitter1Geometry = new THREE.SphereGeometry(.5, 32, 32);
+        this.hitter1Material = new THREE.MeshBasicMaterial({ color: "red" });
+        this.hitter1 = new THREE.Mesh(this.hitter1Geometry, this.hitter1Material);
+        this.hitter1.position.set(0, 0, 0);
+        this.scene.add(this.hitter1);
+        this.hitter2Geometry = new THREE.SphereGeometry(.5, 32, 32);
+        this.hitter2Material = new THREE.MeshBasicMaterial({ color: "blue" });
+        this.hitter2 = new THREE.Mesh(this.hitter2Geometry, this.hitter2Material);
+        this.hitter2.position.set(0, 0, 0);
+        this.scene.add(this.hitter2);
     }
 }
