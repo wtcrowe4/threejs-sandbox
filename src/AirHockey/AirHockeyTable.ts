9beac2ee47ea1a286c55fc45ef335fe4bbaa2856
Thomas Crowe <wtcrowe4@outlook.com>
2023-10-22 18:05:25 -0400

    table structure

--- src/AirHockey/AirHockeyTable.ts
+++ src/AirHockey/AirHockeyTable.ts
@@ -24,19 +24,17 @@ export default class AirHockeyTable {
     private hitter2: THREE.Mesh;
     private hitter2Material: THREE.MeshBasicMaterial;
     private hitter2Geometry: THREE.SphereGeometry;
+    private goal1: THREE.Mesh;
+    private goal2: THREE.Mesh;
 
     constructor(scene: AirHockeyScene) {
         this.scene = scene;
-        this.initializeTable();
-        this.initializePieces();
-        
-    }
+        //this.initializeTable();
+        //airhockey table design
+        this.tableDesign = new THREE.Mesh();
+        this.tableDesign.position.set(0, 0, 0);
+        this.scene.add(this.tableDesign);
 
-    public GetTable() {
-        return this.table;
-    }
-
-    public initializeTable() {
         this.tableWidth = 10;
         this.tableHeight = 1;
         this.tableDepth = 10;
@@ -50,10 +48,7 @@ export default class AirHockeyTable {
         this.table = new THREE.Mesh(this.tableGeometry, this.tableMaterial);
         this.table.position.set(this.tableX, this.tableY, this.tableZ);
         this.scene.add(this.table);
-        
-    }
-
-    public initializePieces() {
+        //this.initializePieces();
         this.puckGeometry = new THREE.SphereGeometry(.5, 32, 32);
         this.puckMaterial = new THREE.MeshBasicMaterial({ color: "black" });
         this.puck = new THREE.Mesh(this.puckGeometry, this.puckMaterial);
@@ -69,5 +64,29 @@ export default class AirHockeyTable {
         this.hitter2 = new THREE.Mesh(this.hitter2Geometry, this.hitter2Material);
         this.hitter2.position.set(0, 0, 0);
         this.scene.add(this.hitter2);
+        const goal1Geometry = new THREE.BoxGeometry(1, 1, 1);
+        const goal1Material = new THREE.MeshBasicMaterial({ color: "red" });
+        this.goal1 = new THREE.Mesh(goal1Geometry, goal1Material);
+        this.goal1.position.set(0, 0, 0);
+        this.scene.add(this.goal1);
+        const goal2Geometry = new THREE.BoxGeometry(1, 1, 1);
+        const goal2Material = new THREE.MeshBasicMaterial({ color: "blue" });
+        this.goal2 = new THREE.Mesh(goal2Geometry, goal2Material);
+        this.goal2.position.set(0, 0, 0);
+        this.scene.add(this.goal2);
+
     }
+
+    public GetTable() {
+        return this.table;
+    }
+
+    // public initializeTable() {
+        
+        
+    // }
+
+    // public initializePieces() {
+        
+    // }
 }
