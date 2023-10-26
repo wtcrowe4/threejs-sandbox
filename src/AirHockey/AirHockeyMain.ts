a8151c4f10adeee0ebcec810c2e5fcaa9002d009
Thomas Crowe <wtcrowe4@outlook.com>
2023-10-26 07:56:07 -0400

    start on update funtion

--- src/AirHockey/AirHockeyMain.ts
+++ src/AirHockey/AirHockeyMain.ts
@@ -30,7 +30,29 @@ export default class AirHockeyGame {
     }
 
     public Update() {
-        // air hockey update logic here
+        // Get the current time
+        const time = Date.now();
+
+        // Update the puck position
+        this.scene.puck.update(time);
+
+        // Update the player paddles
+        this.scene.player1.update(time);
+        this.scene.player2.update(time);
+
+        // Check for collisions between the puck and the players
+        if (this.scene.puck.collidesWith(this.scene.player1)) {
+            this.scene.puck.handleCollision(this.scene.player1);
+        }
+        if (this.scene.puck.collidesWith(this.scene.player2)) {
+            this.scene.puck.handleCollision(this.scene.player2);
+        }
+
+        // Check for goals
+        if (this.scene.puck.isGoalScored()) {
+            // Handle goal scored
+            this.scene.handleGoalScored();
+        }
     }
 
 }
